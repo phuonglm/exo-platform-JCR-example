1bb1180654d54664e95ef25276e02c8b3475fb42
Ly Minh Phuong <ly.minh.phuong.1403@gmail.com>
2011-03-22 18:42:57 +0700

    change service

---
+++ src/main/java/org/exoplatform/jcr/example/common/NodeProperties.java
@@ -0,0 +1,6 @@
+package org.exoplatform.jcr.example.common;
+
+public final class NodeProperties {
+	public static final String EXAMPLE_BOOK_TITLE = "example:title";
+	public static final String EXAMPLE_PERSON_NAME = "example:name";
+}
---
+++ src/main/java/org/exoplatform/jcr/example/common/NodeTypes.java
@@ -0,0 +1,6 @@
+package org.exoplatform.jcr.example.common;
+
+public final class NodeTypes {
+	public static final String EXAMPLE_BOOK = "example:book";
+	public static final String EXAMPLE_PERSON = "example:person";
+}
